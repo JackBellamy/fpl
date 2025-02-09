aaa7ce552a0922f887a73a4f5184388d4bfd0aba
Jack <cc20444@bristol.ac.uk>
2025-02-09 19:21:03 +0000

    changes

--- scrape_fixtures.py
+++ scrape_fixtures.py
@@ -10,18 +10,7 @@ import sys
 
 import csv
 import re
-
-#def split_match_info(match_string):
-    # # Use regex to extract parts
-    # match_pattern = r"(.+) vs\. (.+) - (\w{3} \d{1,2} \w{3}) (\d{2}:\d{2})"
-    # match = re.match(match_pattern, match_string)
-
-    # if match:
-    #     home_team, away_team, date, time = match.groups()
-    #     print(home_team, away_team, date, time)
-    #     return home_team.strip(), away_team.strip(), date.strip(), time.strip()
-    # else:
-    #     return "","","",""
+from datetime import datetime
 
 async def main():
     async with aiohttp.ClientSession() as session:
@@ -32,18 +21,18 @@ async def main():
 
         for i in range(1, len(fixtures),1):
             fixture = fixtures[i]
-            print(str(fixture))
             # Example usage
 
             # Use regex to extract parts
             match_pattern = r"(.+) vs\. (.+) - (\w{3} \d{1,2} \w{3}) (\d{2}:\d{2})"
             match = re.match(match_pattern, str(fixture))
 
-            if match:
-                home_team, away_team, date, time = match.groups()
-                striped_fixtures.append([date.strip(), time.strip(), home_team.strip(), away_team.strip()])
-            else:
-                striped_fixtrues.append(["","","",""])
+            home_team, away_team, date, time = match.groups()
+            date_obj = datetime.strptime(date.strip(), "%a %d %b")
+
+            # Format the datetime object to the desired format: %d-%m
+            formatted_date = date_obj.strftime("%d-%m")
+            striped_fixtures.append([formatted_date, time.strip(), home_team.strip(), away_team.strip()])
 
             # Writing to CSV
         with open("fixtures.csv", mode="w", newline="") as file:
@@ -51,7 +40,6 @@ async def main():
             writer.writerow(["Date", "Time","Home Team", "Away Team"])  # Header
             writer.writerows(striped_fixtures)  # Data row
 
-        print("Done")
 
 if __name__ == "__main__":
     asyncio.run(main())
